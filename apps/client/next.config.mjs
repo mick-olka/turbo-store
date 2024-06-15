ac9ee60c7f450be238b295cac5b20b73d8f7a060
mhutsal <mhutsal@circle.cloud>
2024-06-15 20:12:13 +0300

    upd: next config for new url

--- apps/client/next.config.mjs
+++ apps/client/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [{ hostname: "rotang.ua" }, { hostname: "localhost" }, { hostname: "178.54.240.228" }],
+    remotePatterns: [{ hostname: "rotang.ua" }, { hostname: "localhost" }, { hostname: "178.54.240.228" }, { hostname: "109.248.232.62" }],
   },
 };
 
