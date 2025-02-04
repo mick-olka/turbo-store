c052485a3e7ad09d8b4f509d5e5364dc101a1cc0
mick-olka <mickolka64@gmail.com>
2025-02-04 20:29:20 +0000

    upd: next.config

--- apps/client/next.config.mjs
+++ apps/client/next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   images: {
     remotePatterns: [
       { hostname: "rotang.ua" },
+      { hostname: "papasan.pp.ua" },
       { hostname: "localhost" },
       { hostname: "178.54.240.228" },
       { hostname: "109.248.232.62" },
